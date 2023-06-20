47f5ead12b5bb3b764236381c2a81a9c1907cd54
flheck <fh.it@aikq.de>
2023-06-20 18:07:13 +0700

    feat: init - history service
    Initialize a new history service

---
+++ contracker_history/test/contracker_history.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ContrackerHistory from '../lib/contracker_history-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/contracker_history-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ContrackerHistory.ContrackerHistoryStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
